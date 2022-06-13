92186f51a73b5b5b0a511b46b9b1b7eec55af5cb
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-06-13 14:04:30 +0300

    Add files via upload

--- src/commands/setchannel.ts
+++ src/commands/setchannel.ts
@@ -1,6 +1,6 @@
 import { ICommand } from 'wokcommands' 
 import fs from 'fs'
-
+import { MessageActionRow,MessageButton } from 'discord.js'
 
 export default {
     names: 'setchannel',
@@ -11,22 +11,45 @@ export default {
     minArgs: 1,
     maxArgs: 1,
     cooldown: '60s',
-    callback: ({message, interaction, channel}) => {
-        fs.readFile('./channels.json','utf-8',(err,data) => {
-            if (err){
-                console.log(err)
-                return
-            }
-            let json = JSON.parse(data)
+    callback: async ({message, interaction, channel}) => {
+        if (channel.type !== 'GUILD_TEXT'){
+            const row = new MessageActionRow()
+            .addComponents(
+				new MessageButton()
+					.setCustomId('en')
+					.setEmoji(':flag_gb:')
+					.setStyle('PRIMARY')
+			)
+            let collector
             if (interaction){
-                json[interaction.guild!.id] = channel.id
-                interaction.reply('Channel changed!')
+                collector = interaction.channel?.createMessageComponentCollector()
+                await interaction.reply({content: 'Це має бути текстовий канал!',components: [row]})
             } else {
-                json[message.guild!.id] = channel.id
-                message.reply('Channel changed!')
+                await message.channel.send({content: 'Це має бути текстовий канал!',components: [row]})
             }
-            json = JSON.stringify(json,null,4)
-            fs.writeFile('channels.json',json,(err) => console.log(err))
-        })
+            collector?.on('collect', async id => {
+                if (id.customId === 'en') {
+                    await id.update({ content: 'This must be text channel!', components: [row]});
+                }
+            })
+
+        } else {
+            fs.readFile('./channels.json','utf-8',async (err,data) => {
+                if (err){
+                    console.log(err)
+                    return
+                }
+                let json = JSON.parse(data)
+                if (interaction){
+                    json[interaction.guild!.id] = channel.id
+                    await interaction.reply('Channel changed!')
+                } else {
+                    json[message.guild!.id] = channel.id
+                    await message.reply('Channel changed!')
+                }
+                json = JSON.stringify(json,null,4)
+                fs.writeFile('channels.json',json,(err) => console.log(err))
+            })
+        }
     }
 } as ICommand
