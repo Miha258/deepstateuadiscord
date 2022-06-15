fce995e02bdffd6f01c4d242eb56c54529c83a72
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-06-15 23:25:08 +0300

    Add files via upload

--- src/commands/setchannel.ts
+++ src/commands/setchannel.ts
@@ -1,6 +1,6 @@
 import { ICommand } from 'wokcommands' 
 import fs from 'fs'
-import { MessageActionRow,MessageButton,Message} from 'discord.js'
+import { CacheType, InteractionCollector, MessageActionRow,MessageButton, MessageComponentInteraction} from 'discord.js'
 
 export default {
     names: 'setchannel',
@@ -11,26 +11,37 @@ export default {
     minArgs: 1,
     maxArgs: 1,
     cooldown: '60s',
-    callback: async ({message, interaction, channel}) => {
-        if (channel.type !== 'GUILD_TEXT'){
-            const row = new MessageActionRow()
-            .addComponents(
-				new MessageButton()
-					.setCustomId('en')
-					.setEmoji(':flag_gb:')
-					.setStyle('PRIMARY')
-			)
-            let collector
+    callback: async ({message, interaction,client,args}) => {
+        const channel = client.channels.cache.get(args[0])
+        let collector: InteractionCollector<MessageComponentInteraction<CacheType>> | undefined 
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setEmoji('🇬🇧')
+                .setCustomId('en')
+                .setStyle('PRIMARY')
+        ).addComponents(
+            new MessageButton()
+                .setEmoji('🇺🇦')
+                .setCustomId('ua')
+                .setStyle('PRIMARY')
+        )
+        if (channel?.type !== 'GUILD_TEXT'){
+            collector = interaction.channel?.createMessageComponentCollector()
             if (interaction){
-                collector = interaction.channel?.createMessageComponentCollector()
                 await interaction.reply({content: 'Це має бути текстовий канал!',components: [row]})
             } else {
-                collector = message.channel?.createMessageComponentCollector()
                 await message.channel.send({content: 'Це має бути текстовий канал!',components: [row]})
             }
             collector?.on('collect', async id => {
                 if (id.customId === 'en') {
-                    await id.update({ content: 'This must be text channel!', components: [row]});
+                    row.components[1].setDisabled(false)
+                    row.components[0].setDisabled(true)
+                    await id.update({ content: 'This must be text channel!', components: [row]})
+                } else if (id.customId === 'ua') {
+                    row.components[0].setDisabled(false)
+                    row.components[1].setDisabled(true)
+                    await id.update({ content: 'Це має бути текстовий канал!', components: [row]})
                 }
             })
 
@@ -41,13 +52,24 @@ export default {
                     return
                 }
                 let json = JSON.parse(data)
+                json[interaction.guild!.id] = channel.id
                 if (interaction){
-                    json[interaction.guild!.id] = channel.id
-                    await interaction.reply('Channel changed!')
+                    await interaction.reply({content: 'Канал змінено', components: [row]})
                 } else {
-                    json[message.guild!.id] = channel.id
-                    await message.reply('Channel changed!')
+                    await message.reply({content: 'Канал змінено', components: [row]})
                 }
+                collector = interaction.channel?.createMessageComponentCollector()
+                collector?.on('collect', async id => {
+                    if (id.customId === 'en') {
+                        row.components[1].setDisabled(false)
+                        row.components[0].setDisabled(true)
+                        await id.update({ content: 'Channel changed', components: [row]})
+                    } else if (id.customId === 'ua') {
+                        row.components[0].setDisabled(false)
+                        row.components[1].setDisabled(true)
+                        await id.update({ content: 'Канал змінено', components: [row]})
+                    }
+                })
                 json = JSON.stringify(json,null,4)
                 fs.writeFile('channels.json',json,(err) => console.log(err))
             })
